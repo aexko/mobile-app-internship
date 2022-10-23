f21589a3ca0ac264c6dacfcef45996b8c83a23e0
Alex Kao <alexkxosb@gmail.com>
2022-10-23 18:03:34 -0400

    Maps: ok

--- Zoeken/app/src/main/java/ca/qc/bdeb/c5gm/zoeken/MainActivity.java
+++ Zoeken/app/src/main/java/ca/qc/bdeb/c5gm/zoeken/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
+
+    public void ouvrirConnexion(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
