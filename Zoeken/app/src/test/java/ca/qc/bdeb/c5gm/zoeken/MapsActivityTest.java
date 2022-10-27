60adb83dded88f90561e348a74fbc584a0c8a6fc
Alex Kao <alexkxosb@gmail.com>
2022-10-26 23:25:02 -0400

    Début des tests (à voir si je les fais)

---
+++ Zoeken/app/src/test/java/ca/qc/bdeb/c5gm/zoeken/MapsActivityTest.java
@@ -0,0 +1,14 @@
+package ca.qc.bdeb.c5gm.zoeken;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MapsActivityTest {
+    public class ExampleUnitTest {
+        @Test
+        public void addition_isCorrect() {
+            assertEquals(4, 2 + 2);
+        }
+    }
+}
---
+++ Zoeken/app/src/test/java/ca/qc/bdeb/c5gm/zoeken/StockageTest.java
@@ -0,0 +1,17 @@
+package ca.qc.bdeb.c5gm.zoeken;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StockageTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void TearDown() {
+
+    }
+ }
