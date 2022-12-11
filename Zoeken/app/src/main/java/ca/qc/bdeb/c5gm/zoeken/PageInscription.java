6ffe36fa7308cbed1cf2e27f10d5a833a2ce3e21
Alex Kao <ab.kao@hotmail.com>
2022-12-11 15:07:01 -0500

    Page inscription UI

--- Zoeken/app/src/main/java/ca/qc/bdeb/c5gm/zoeken/PageInscription.java
+++ Zoeken/app/src/main/java/ca/qc/bdeb/c5gm/zoeken/PageInscription.java
@@ -10,5 +10,7 @@ public class PageInscription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_inscription);
+
+
     }
 }
